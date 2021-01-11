8577c51ba073a53cf59f209013f59633e2277f73
Christopher Artrip <christopherartrip@christophersmbp.home>
2021-01-10 21:43:18 -0500

    first commit, setting things up

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Note from './components/Note'
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Footer />
+      <Note />
+    </div>
+  );
+}
+
+export default App;
